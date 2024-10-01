d0d73fd86e33afc9a5c7ed32eaf16612dd244bf7
lejlahadzovic <hadzoviclejla6@gmail.com>
2024-10-01 21:26:48 +0200

    Remove unused variables

--- StudentOglasi/StudentOglasi.Services/Services/MailService.cs
+++ StudentOglasi/StudentOglasi.Services/Services/MailService.cs
@@ -12,15 +12,10 @@ namespace StudentOglasi.Services
 {
     public class MailService : IMailService
     {
-        private readonly string serverAddress = Environment.GetEnvironmentVariable("SERVER_ADDRESS");
-        private readonly string mailSender = Environment.GetEnvironmentVariable("MAIL_SENDER");
-        private readonly string mailPass = Environment.GetEnvironmentVariable("MAIL_PASS");
-        private readonly int port = int.Parse(Environment.GetEnvironmentVariable("MAIL_PORT"));
         public async Task startConnection(EmailObject obj)
         {
             var hostname = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
             var factory = new ConnectionFactory { HostName = hostname };
-            //UserName = username, Password = password };
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
             channel.QueueDeclare(queue: "email_sending",
